3e0025a7d605dbe9e31659b9b3cf62d348c6660e
Gairus Niel <nielstupidity@gmail.com>
2024-11-20 22:43:12 +0800

    fixed corrupted lvl_data resources

--- code-teacher/levels/level_data.gd
+++ code-teacher/levels/level_data.gd
@@ -4,7 +4,6 @@ extends Resource
 
 @export_range(0,10) var level_number : int
 @export_range(0,2) var quiz_level_number : int = 0
-@export var level_scene : PackedScene
 @export_multiline var level_title : String
 @export_multiline var level_objectives : String
 @export_multiline var level_skills : String
